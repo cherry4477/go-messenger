d8b8d4e63830c63bc6c3ced2b7eecc0025a771fe
cherry4477 <cherry4477@users.noreply.github.com>
2016-05-25 23:52:34 +0800

    Update compile.sh

--- build/1.5/compile.sh
+++ build/1.5/compile.sh
@@ -3,6 +3,7 @@ set -e
 # Set directory to where we expect code to be
 cd /go/src/${SOURCE_PATH}
 echo "Downloading dependencies"
+chmod u+x Godeps.json
 godep restore
 echo "Fix formatting"
 go fmt ./...
